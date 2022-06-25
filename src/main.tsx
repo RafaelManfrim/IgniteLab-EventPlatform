749d35bdadc8918cfc36e442934de5c795b13614
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-06-25 11:39:10 -0300

    Aprimorando visual da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { ApolloProvider } from '@apollo/client'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-
-import { client } from './lib/apollo'
 import App from './App'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
